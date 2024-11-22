f42d4bfa9ebceeebf82c536ed0729a9f54484214
Mayankkant08 <163098491+Mayankkant08@users.noreply.github.com>
2024-11-22 12:23:52 +0530

    app.js

--- app.js
+++ app.js
@@ -5,10 +5,10 @@ let search = document.getElementByClassName('cards')[0];
 let search_input = document.getElementById('search_input');
 
 left_btn.addEventListener('click', () => {
-    cards.srcollLeft -= 140;
+    cards.scrollLeft -= 140;
 })
 right_btn.addEventListener('click', () => {
-    cards.srcollLeft += 140;
+    cards.scrollLeft += 140;
 })
 
 let json_url = "movie.json";
@@ -23,7 +23,7 @@ fetch(json_url).then(Response => Response.json())
             card.innerHTML = `
             <img src="${sposter}" alt="${name}" class="poster">
             <div class="rest_card">
-                <img src="${bposter}" alt="" class="poster">
+                <img src="${bposter}" alt="">
                 <div class="cont">
                     <h4>${name}</h4>
                     <div class="sub">
@@ -37,9 +37,9 @@ fetch(json_url).then(Response => Response.json())
         });
 
         document.getElementById('title').innerText = data[0].name;
-        document.getElementById('gen').innerText = data[0].name;
-        document.getElementById('date').innerText = data[0].name;
-        document.getElementById('rate').innerText = `</span><i class="bi bi-play-fill"></i> ${data[0].imdb}`
+        document.getElementById('gen').innerText = data[0].genre;
+        document.getElementById('date').innerText = data[0].date;
+        document.getElementById('rate').innerHTML = `</span><i class="bi bi-play-fill"></i> ${data[0].imdb}`
 
         //search data load
         data.forEach(element => {
@@ -66,14 +66,14 @@ fetch(json_url).then(Response => Response.json())
                 let b = a[index].getElementByClassName('cont')[0];
                 // console.log(a.textContent)
                 let TextValue = b.textContent || b.innerText;
-                if(TextValue.toUpperCase().indexOf(filter)){
+                if(TextValue.toUpperCase().indexOf(filter) > -1){
                     a[index].style.display = "flex";
                     search.style.visibility = "visible";
                     search.style.opacity = 1;
                 }else{
                     a[index].style.display = "none";
                 }
-                if(search_input == 0){
+                if(search_input.value == 0){
                     search.style.visibility = "hidden";
                     search.style.opacity = 0;
                 }
@@ -88,7 +88,7 @@ fetch(json_url).then(Response => Response.json())
                 play.innerHTML = `Play <i class="bi bi-pause-fill"></i>`
             }else{
                 video.pause();
-                play.innerHTML = `Watch<i class="bi bi-play-fill"></i>`
+                play.innerHTML = `Watch <i class="bi bi-play-fill"></i>`
             }
         })
 
